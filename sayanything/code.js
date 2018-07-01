9ea67e807f9fedf27804403264767742ef784624
cardflopper <smchacko@gmail.com>
2018-07-01 04:07:04 -0400

    Update code.js

--- sayanything/code.js
+++ sayanything/code.js
@@ -4,8 +4,8 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function nextQuestion(){
-    var i = getRandomInt(0,questions.length-1)
+function nextQuestion() {
+    var i = getRandomInt(0,questions.length-1);
     
     var q = document.getElementById("question");
     q.innerHTML = questions[i];
